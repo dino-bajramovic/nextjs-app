4cba668ea051b6546dc9207e0e907976dda2eeaf
Dino Bajramović <dbajramovi3@etf.unsa.ba>
2025-04-20 23:47:28 +0200

    test2

--- src/drizzle/schema/product.ts
+++ src/drizzle/schema/product.ts
@@ -15,6 +15,7 @@ export const ProductTable = pgTable("products", {
   priceInDollars: integer().notNull(),
   status: productStatusEnum().notNull().default("private"),
   createdAt,
+  updatedAt,
 });
 //podsjetnik: 
 //u drizzle ne mozemo napraviti many to many relations, moramo napraviti between tabelu: "courseProduct.ts pr"
